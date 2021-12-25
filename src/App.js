7d4e3554e615cbc9abbf6853194806bd2cf1c542
Artree <narise378@gmail.com>
2021-12-25 17:48:51 +0900

    learn useEffect useRef

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import React from 'react'
 // import UseState from './tutorial/1-useState/setup/2-useState-basics'
 // import UseArray from './tutorial/1-useState/setup/3-useState-array'
 // import UseObject from './tutorial/1-useState/setup/4-useState-object'
-import Counter from './tutorial/1-useState/setup/5-useState-counter'
+// import Counter from './tutorial/1-useState/setup/5-useState-counter'
+// import Effect from './tutorial/2-useEffect/setup/1-useEffect-basics'
+// import Cleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+// import Fetch from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+// import Conditional from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
+// import Short from './tutorial/3-conditional-rendering/setup/2-short-circuit'
+// import SH from './tutorial/3-conditional-rendering/setup/3-show-hide'
+// import Forms from './tutorial/4-forms/setup/1-controlled-inputs'
+// import Forms from './tutorial/4-forms/setup/2-multiple-inputs'
+import Ref from './tutorial/5-useRef/setup/1-useRef-basics'
 
 function App() {
   return (
     <div className='container'>
-      <Counter />
+      <Ref />
     </div>
   )
 }
